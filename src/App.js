8860595a2e7dd3253f297a89e8dfbb882f2870e3
Tariq Nazar <tariqfnazar@gmai.com>
2018-04-25 19:57:56 +0200

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string';
+
 
 class Playlist extends Component {
   render() {
     return (
-      <h2>This is a playlist</h2>
+      <div className='playlist'>
+        <div className="title">
+          <h2>Title</h2>
+        </div>
+        <div className="songs">
+          <ul>
+            <li>song1</li>
+            <li>song2</li>
+            <li>song3</li>
+          </ul>
+        </div>
+      </div>
     );
   }
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      serverData :{}
+    }
+  }
+  componentDidMount(){
+    let parsed = queryString.parse(window.location.search)
+    let the_access_token = parsed.access_token;
+    
+    fetch('https://api.spotify.com/v1/me', 
+    {headers:{'Authorization': 'Bearer ' + the_access_token}})
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+   
+  }
   render() {
     return (
       <div>
